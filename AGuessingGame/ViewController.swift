5188794973697c5e38ad9d6afbe3956bda4fb8a2
Maxwell Lu <blu@lcs.on.ca>
2019-10-03 21:08:21 -0400

    Initial Commit

---
+++ AGuessingGame/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AGuessingGame
+//
+//  Created by Maxwell Lu on 2019-10-03.
+//  Copyright © 2019 Maxwell Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
